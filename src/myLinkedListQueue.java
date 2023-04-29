798a0eb873c95e42a8379abbc2d3103d4966a346
allister3879 <220956@astanait.edu.kz>
2023-04-29 12:19:06 +0600

    implementation of constructor

--- src/myLinkedListQueue.java
+++ src/myLinkedListQueue.java
@@ -1,5 +1,7 @@
 import java.util.NoSuchElementException;
 public class myLinkedListQueue {
     MyLinkedList list = new MyLinkedList<>();
-
+    public myLinkedListQueue(){
+        list = new MyLinkedList<>();
+    }
 }
