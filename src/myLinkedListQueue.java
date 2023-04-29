03ffef509ea12438b47fc605b18af48c2e7ad4da
allister3879 <220956@astanait.edu.kz>
2023-04-29 12:22:29 +0600

    implementation of enqueue method

--- src/myLinkedListQueue.java
+++ src/myLinkedListQueue.java
@@ -1,7 +1,11 @@
 import java.util.NoSuchElementException;
-public class myLinkedListQueue {
+public class myLinkedListQueue<E> {
     MyLinkedList list = new MyLinkedList<>();
     public myLinkedListQueue(){
         list = new MyLinkedList<>();
+    }
+
+    public void enqueue(E element){
+        list.add(element); // add element to the list
     }
 }
