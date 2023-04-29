90e55eafb2478a4964afe902b0c5fce35c3cf8f5
allister3879 <220956@astanait.edu.kz>
2023-04-29 11:31:09 +0600

    test for push method - stack

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         myLinkedListStack stack = new myLinkedListStack();
+        System.out.println("--- Test for 'push' method ---");
+        stack.push(1);
+        stack.push('a');
+        stack.push(7.7);
     }
 }
